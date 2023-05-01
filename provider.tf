666718a67cb1d0a06a7d650890ec9103eab1c43d
Suraj <suraj.solanki@spinny.com>
2023-05-01 14:11:16 +0530

    removed state locking

--- provider.tf
+++ provider.tf
@@ -26,8 +26,8 @@ terraform {
     bucket         = "terraform-state-bucket"
     key            = "infra/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
+    # dynamodb_table = "terraform-state-lock-table"
+    # encrypt        = true
   }
 }
 
